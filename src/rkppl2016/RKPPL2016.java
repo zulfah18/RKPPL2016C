6ac9ec17d4d41530f1519030e242c6090fd3f561
zulfahnur <zulfahnur.18@mail.unpas.ac.id>
2016-03-04 16:32:33 +0700

    second commit

--- src/rkppl2016/RKPPL2016.java
+++ src/rkppl2016/RKPPL2016.java
@@ -17,6 +17,7 @@ public class RKPPL2016 {
     public static void main(String[] args) {
         
         System.out.println("test");
+        System.out.println("test2");
         // TODO code application logic here
     }
     
