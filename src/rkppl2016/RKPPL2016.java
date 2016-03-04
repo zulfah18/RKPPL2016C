65acff04a85945016202fdeeb1b507671f665bb5
zulfahnur <zulfahnur.18@mail.unpas.ac.id>
2016-03-04 16:29:53 +0700

    first commit

---
+++ src/rkppl2016/RKPPL2016.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rkppl2016;
+
+/**
+ *
+ * @author Assisten
+ */
+public class RKPPL2016 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("test");
+        // TODO code application logic here
+    }
+    
+}
